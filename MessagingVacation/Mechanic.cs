b6114f67874aed39b95569643f19ae8de68b3082
Nemus <Nemus@DESKTOP-VUL10HB>
2019-05-30 18:13:51 +0200

    Vacation knows how mechanic does it

---
+++ MessagingVacation/Mechanic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessagingVacation
+{
+    class Mechanic
+    {
+        internal void CheckBrakes(Bus bus)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void CheckEngine(Bus bus)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void CheckTires(Bus bus)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MessagingVacation/Vacation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MessagingVacation
+{
+    class Vacation
+    {
+        public Vacation(Bus bus)
+        {
+            Bus = bus ?? throw new ArgumentNullException("bus");
+        }
+
+        public Bus Bus { get; }
+
+        public void PrepareVacation(Mechanic mechanic)
+        {
+            mechanic.CheckBrakes(Bus);
+            mechanic.CheckEngine(Bus);
+            mechanic.CheckTires(Bus);
+        }
+    }
+}
