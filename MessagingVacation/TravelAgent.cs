3646aff157e87c5aa7c2aea9185eb23ccd86cbfc
Nemus <Nemus@DESKTOP-VUL10HB>
2019-06-07 22:36:40 +0200

    TravelAgent

---
+++ MessagingVacation/TravelAgent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessagingVacation
+{
+    class TravelAgent
+    {
+        internal void BookHotel(List<Tourist> tourists)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MessagingVacation/Vacation.cs
+++ MessagingVacation/Vacation.cs
@@ -1,19 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MessagingVacation
 {
     class Vacation
     {
-        public Vacation(Bus bus)
+        public Vacation(Bus bus, List<Tourist> tourists)
         {
             Bus = bus ?? throw new ArgumentNullException("bus");
+            Tourists = tourists ?? throw new ArgumentNullException("tourists");
         }
 
         public Bus Bus { get; }
+        public List<Tourist> Tourists { get; }
 
-        public void PrepareVacation(Mechanic mechanic)
+        public void PrepareVacation(Mechanic mechanic, TravelAgent travelAgent)
         {
             mechanic.PrepareVacation(Bus);
+            travelAgent.BookHotel(Tourists);
         }
     }
 }
